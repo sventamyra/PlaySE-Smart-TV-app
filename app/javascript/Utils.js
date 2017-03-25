c71adb348578cb238383eb93de19089946fb8242
JeppeTh <JeppeTh@users.noreply.github.com>
2017-03-25 11:11:04 +0100

    Fix for SVT Live

--- app/javascript/Utils.js
+++ app/javascript/Utils.js
@@ -463,10 +463,12 @@ checkDateFormat = function() {
 }
 
 timeToDate = function(timeString) {
-    if (dateFormat == 1)
-        timeString = timeString.replace(/-/g,"/").replace("T", " ").replace(/\+([0-9]+):([0-9]+)/,"+$1$2").replace("Z", "+0000")
-    else
-        timeString = timeString.replace(/-/g," ").replace("T", " ").replace(/\+/," +").replace("Z", " +00:00")
+    if (+timeString != timeString){
+        if (dateFormat == 1)
+            timeString = timeString.replace(/-/g,"/").replace("T", " ").replace(/\+([0-9]+):([0-9]+)/,"+$1$2").replace("Z", "+0000")
+        else
+            timeString = timeString.replace(/-/g," ").replace("T", " ").replace(/\+/," +").replace("Z", " +00:00")
+    }
     var date = new Date(timeString);
     return new Date(date.getTime() + dateOffset);
 }
